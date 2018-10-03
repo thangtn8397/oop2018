a60fcc7b84c7461c11817805f18f26f9801aad95
thangtn <nhuthang.tb@gmail.com>
2018-10-03 14:20:01 +0700

    week3

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -4,12 +4,18 @@ public class Week3 {
 
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+
+        return (m > n ? m : n);
     }
 
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int n = array.length;
+        int min = array[0];
+        for(int i=0; i<n; i++){
+            if(min > array[i]) min = array[i];
+        }
+        return min;
     }
 
     /**
@@ -20,6 +26,12 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        String result;
+        double ibm = (weight)/ (height*height);
+        if(ibm < 18.5)  result = "Thieu can";
+        else if(ibm >=18.5 && ibm <= 24.9) result = "Binh thuong";
+        else if(ibm >= 25 && ibm <= 29.5) result = "Thua can";
+        else result = "Beo phi";
+        return result;
     }
 }
--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -1,10 +1,42 @@
 package week3;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Week3Test {
 
     // TODO: Viết 5 testcase cho phương thức max()
 
+    public void testMax(){
+        assertEquals(4,Week3.max(4, 5));
+        assertEquals(3724,Week3.max(3724,3725));
+        assertEquals(10,Week3.max(10,20));
+        assertEquals(0,Week3.max(0,1));
+        assertEquals(5,Week3.max(5,30));
+    }
+
     // TODO: Viết 5 testcase cho phương thức minOfArray()
 
+    public void testMinOfArray(){
+        int[] arr = {2,1,4,7,8};
+        assertEquals(1,Week3.minOfArray(arr));
+        int[] arr1 = {5,1000,123,6};
+        assertEquals(5,Week3.minOfArray(arr1));
+        int[] arr2 = {56,4,6,7};
+        assertEquals(4,Week3.minOfArray(arr2));
+        int[] arr3 = {4,5,10,400,90};
+        assertEquals(4,Week3.minOfArray(arr3));
+        int[] arr4 = {5,100,3,0,7};
+        assertEquals(0, Week3.minOfArray(arr4));
+    }
+
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
+    public void testcalculateBMI(){
+        assertEquals("Binh thuong", Week3.calculateBMI(60,1.7));
+        assertEquals("Beo phi", Week3.calculateBMI(100,1.5));
+        assertEquals("Thua can", Week3.calculateBMI(70,1.6));
+        assertEquals("Thieu can", Week3.calculateBMI(40,1.7));
+        assertEquals("Thieu can", Week3.calculateBMI(30,1.5));
+
+    }
 }
